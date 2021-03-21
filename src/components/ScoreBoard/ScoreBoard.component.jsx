912ef5879013e45eb188d8acf1a02f87a7d68a5c
Michael Sasonker <michaelsas1212@gmail.com>
2021-03-21 02:01:42 +0200

    update

--- src/components/ScoreBoard/ScoreBoard.component.jsx
+++ src/components/ScoreBoard/ScoreBoard.component.jsx
@@ -27,28 +27,5 @@ class ScoreBoard extends React.Component {
     );
   }
 }
-//   render() {
-//     return (
-//       <ul className="scoreboard">
-//         <li key='target' className="target-score">
-//           <h3>Target Score: {this.props.targetScore}</h3>
-//         </li>
-//         <li key='scores' className="players-score">
-//           {this.props.players
-//             .filter((player, index) => index !== this.props.currentPlayerIndex)
-//             .map((player) => {
-//               return (
-//                 <li key='score' className="player-score">
-//                   <h4>{player.name}</h4>
-//                   <div>Total Score: {player.totalScore}</div>
-//                   <div>Won rounds: {player.wins}</div>
-//                 </li>
-//               );
-//             })}
-//         </li>
-//       </ul>
-//     );
-//   }
-// }
 
 export default ScoreBoard;
